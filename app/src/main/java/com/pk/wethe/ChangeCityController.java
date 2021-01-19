4c018c45ebaeed7384bf29c3fbe22affcf2c198e
Nit89 <nitesh382singh@gmail.com>
2021-01-20 01:33:36 +0530

    INIT

---
+++ app/src/main/java/com/pk/wethe/ChangeCityController.java
@@ -0,0 +1,54 @@
+package com.pk.wethe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChangeCityController extends AppCompatActivity {
+
+    ImageButton backButton;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.change_city_layout);
+
+
+        Log.i("kgfkjakfhk","afkjhadfj");
+        backButton = findViewById(R.id.backButton);
+        final EditText editTextField = findViewById(R.id.queryET);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        editTextField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                String newCity = editTextField.getText().toString();
+                Intent newCityIntent = new Intent(ChangeCityController.this, WeatherController.class);
+                newCityIntent.putExtra("city", newCity);
+                startActivity(newCityIntent);
+                return false;
+
+
+            }
+        });
+
+
+    }
+
+
+}
